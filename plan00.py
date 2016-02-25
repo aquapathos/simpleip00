7543217534ac7b95393183b9092660b4b05a142a
aquapathos <firedwater@gmail.com>
2016-02-25 22:29:20 +0900

    Update plan00.py

--- plan00.py
+++ plan00.py
@@ -15,12 +15,10 @@ import numpy as np
 def cv2q4pixmap(cv2img):
     height, width, dim = cv2img.shape
     bytesPerLine = dim * width
-    print height,width,dim,bytesPerLine
     rgbimg = cv2.cvtColor(cv2img,cv2.COLOR_BGR2RGB)
     q4img = QtGui.QImage(rgbimg.data,width,height,bytesPerLine,QtGui.QImage.Format_RGB888)
     pixmap = QtGui.QPixmap.fromImage(q4img)
     return pixmap
-
 # ウィンドウの上部エリア
 class Pane01(QtGui.QWidget):
     def __init__(self, parent=None):
@@ -29,15 +27,15 @@ class Pane01(QtGui.QWidget):
 
     def initial(self):
         # 512x512の真っ黒のダミー画像を作る
-        dummyimg = cv2q4pixmap(np.zeros((512,512,3),dtype=np.uint8))
-        # Label Widget に画像を貼り付ける．Labelを表示すれば画像が表示される       
-        self.imagePane = QtGui.QLabel(u"パネル",parent=self) 
-        self.imagePane.setPixmap(dummyimg)
-        
+        dummypixmap = cv2q4pixmap(np.zeros((512,512,3),dtype=np.uint8))
+        # Label Widget に画像を貼り付ける．Labelを表示すれば画像が表示される
+        self.imagePane = QtGui.QLabel(u"パネル",parent=self)
+        self.imagePane.setPixmap(dummypixmap)
+
         # 自動水平レイアウトで 上記Widetを配置したレイアウトをつくる
-        layout = QtGui.QHBoxLayout()
-        self.setLayout(layout)      
-        layout.addWidget(self.imagePane)
+        self.layout = QtGui.QHBoxLayout()
+        self.setLayout(self.layout)
+        self.layout.addWidget(self.imagePane)
 
 # ウィンドウの下部エリア
 class Pane02(QtGui.QWidget):
@@ -46,12 +44,12 @@ class Pane02(QtGui.QWidget):
         self.initial()
 
     def initial(self):
-        # ボタンWidgetを一つ作る    
+        # ボタンWidgetを一つ作る
         self.fopenButton = QtGui.QPushButton(u"ボタン",parent=self)
-        
+
         # 自動水平レイアウトで 上記Widetを配置したレイアウトをつくる
         layout = QtGui.QHBoxLayout()
-        self.setLayout(layout)      
+        self.setLayout(layout)
         layout.addWidget(self.fopenButton)
 
 def main():
@@ -62,14 +60,16 @@ def main():
 
     # 自動垂直レイアウトに Pane01,Pane02 のインスタンスを貼り付ける
     layout = QtGui.QVBoxLayout()
-    layout.addWidget(Pane01(parent=panels))
-    layout.addWidget(Pane02(parent=panels))
-    
+    pane01 = Pane01(parent=mainPanels)
+    layout.addWidget(pane01)
+    pane02 = Pane02(parent=mainPanels)
+    layout.addWidget(pane02)
+
     # メインパネルにレイアウトしたものを貼り付ける
-    mainPanels.setLayout(layout)    
+    mainPanels.setLayout(layout)
     # メインパネルをウィンドウ中央エリアに取り付け
-    mainWindow.setCentralWidget(panels)
-    
+    mainWindow.setCentralWidget(mainPanels)
+
     #　最後に表示してプログラムを実行　この2行はおきまり
     mainWindow.show()
     app.exec_()
